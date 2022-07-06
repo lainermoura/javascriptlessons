bde254b6e4200a9261c5b97dfcfb10304b8b5b9d
Laine Moura <lainermoura@gmail.com>
2022-07-06 18:56:01 -0300

    calculamdo o imc

---
+++ tabelaIMC/assets/js/main.js
@@ -0,0 +1,46 @@
+// Capturar evento de submit do formulário
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector('#peso')
+  const inputAltura = e.target.querySelector('#altura')
+
+  const peso = Number(inputPeso.value)
+  const altura = Number(inputAltura.value)
+
+  if (!peso) { 
+    setResultado('Peso inválido', false)
+    return
+  }
+  if (!altura) { 
+    setResultado('Altura inválida', false)
+    return
+  }
+  const imc = getImc(peso, altura)
+
+
+console.log(imc);
+
+})
+
+
+function getImc (peso, altura) {
+  const imc = peso / altura ** 2
+  return imc.toFixed(2)
+}
+
+
+function criaP () { 
+  const p = document.createElement('p')
+  return p
+}
+
+function setResultado (msg, isValid) {
+  const resultado = document.querySelector('#resultado')
+  resultado.innerHTML = ''
+  const p = criaP()
+  p.innerHTML = msg
+  resultado.appendChild(p)
+  
+}
