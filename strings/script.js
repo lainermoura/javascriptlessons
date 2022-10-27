b85825bbf599438ce586cd0d485ae98b8ac1aa3c
Laine Moura <lainermoura@gmail.com>
2022-10-27 16:09:10 -0300

    timer funcionando

--- strings/script.js
+++ strings/script.js
@@ -28,17 +28,23 @@
 // console.log(umaString.split(' ', 2));
 
 //**** toUpperCase/toLowerCase *****
-// console.log(umaString.toLowerCase());
+// // console.log(umaString.toLowerCase());
 
-const nome = prompt('Digite seu nome completo')
-document.body.innerHTML += `O seu nome é <strong>${nome}</strong><br />`
-document.body.innerHTML += `O seu nome tem<strong> ${nome.replaceAll(" ", "").length}</strong> letras <br />`
-document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome[1]}</strong><br />`
-document.body.innerHTML += `Qual o primeiro índice da letra a do seu nome: <strong>${nome.indexOf('a')}</strong><br />`
-document.body.innerHTML += `Qual o último índice da letra a do seu nome: <strong>${nome.lastIndexOf('a')}</strong><br />`
-document.body.innerHTML += `As últimas três letras do seu nome são: <strong>${nome.slice(-3)}</strong><br />`
-document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong><br />`
-document.body.innerHTML += `O seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br />`
-document.body.innerHTML += `O seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`
+// const nome = prompt('Digite seu nome completo')
+// document.body.innerHTML += `O seu nome é <strong>${nome}</strong><br />`
+// document.body.innerHTML += `O seu nome tem<strong> ${nome.replaceAll(" ", "").length}</strong> letras <br />`
+// document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome[1]}</strong><br />`
+// document.body.innerHTML += `Qual o primeiro índice da letra a do seu nome: <strong>${nome.indexOf('a')}</strong><br />`
+// document.body.innerHTML += `Qual o último índice da letra a do seu nome: <strong>${nome.lastIndexOf('a')}</strong><br />`
+// document.body.innerHTML += `As últimas três letras do seu nome são: <strong>${nome.slice(-3)}</strong><br />`
+// document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong><br />`
+// document.body.innerHTML += `O seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br />`
+// document.body.innerHTML += `O seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`
 
-
+function aux(a){
+    a =a -1
+    console.log(a);
+    if(a>0)
+    aux(a)
+}
+console.log(aux(5));
